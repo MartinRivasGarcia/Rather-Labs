966ef501f95534a850697075d3dcac6538c0e1e9
MartinRivasGarcia <Martin@RivasGarcia.com.ar>
2023-09-05 22:25:04 -0300

    Se suben avances con graphql

--- React Node/server/index.js
+++ React Node/server/index.js
@@ -1,13 +1,54 @@
-const app = require("./app")
+const express = require('express');
+const graphqlHTTP  = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+
+    type Client {
+        id: Int
+        name: String
+        number: String
+    }
+    type Query {
+        clients: [Client]
+        client(id : Int): Client
+    }
+    type Mutation {
+        addClient(name : String, number : String): Client
+    }
+`);
+
+var clients = []
+var counter = 1
+
+var root =  {
+    clients : () => { return clients },
+
+    client : (data) => {
+        for (let i = 0; i < clients.length; i++) {
+            if (clients[i].id == data.id) {
+                return clients[i]
+            }
+        }
+        return null
+    },
+
+    addClient: (data) => {
+        var c = { 'id':counter, 'name':data.name, 'number': data.number };
+        clients.push(c)
+        counter ++;
+        return c;
+    }
+};
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(3001, ()=>{
     console.log("Server listening port 3001")
 })
-
-/*app.get("/api/classes", function (req,res) {
-    res.send({classes: classes})
-})*/
-
-app.use("/api/users", require('./routes/users'))
-app.use('/api/notes', require('./routes/notes'))
-app.use("/api/classes", require('./routes/classes'))
